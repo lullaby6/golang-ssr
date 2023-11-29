7a00af0fd8d76a1e54bb89abdce6cb418146fb2e
LucianoBrumer <lucianobrumer5@gmail.com>
2023-11-29 15:32:08 -0300

    react cors

--- src/utils/cors.go
+++ src/utils/cors.go
@@ -2,12 +2,12 @@ package utils
 
 import "github.com/gin-gonic/gin"
 
-func Cors(allowedOrigins, allowedMethods string) gin.HandlerFunc {
+func Cors(origin, methods string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", allowedOrigins)
+		c.Header("Access-Control-Allow-Origin", origin)
+		c.Header("Access-Control-Allow-Methods", methods)
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", allowedMethods)
 
 		c.Next()
 	}
